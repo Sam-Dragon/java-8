0cc27b1032a75936dc6758c9acbd7153e9e9be92
Admin <Admin@host.docker.internal>
2021-09-07 21:40:22 +0530

    Additions

---
+++ Java-8-Optional/src/learn/java8/optional/misc/OptionalThrowingException.java
@@ -0,0 +1,12 @@
+package learn.java8.optional.misc;
+
+import java.util.Optional;
+
+public class OptionalThrowingException {
+
+    public static void main(String[] args) {
+
+        Optional<String> emptyOptional = Optional.empty();
+        emptyOptional.get();
+    }
+}
