44954fd6552afdf93a15bad55c361db95957a0d0
Admin <Admin@host.docker.internal>
2021-09-09 20:47:46 +0530

    Java 8 Parallel streams added

---
+++ Java-8-Parallel-Streams/src/misc/pack/ProcessorCount.java
@@ -0,0 +1,9 @@
+package misc.pack;
+
+public class ProcessorCount {
+
+    public static void main(String[] args) {
+
+        System.out.println("No. of processors :: " + Runtime.getRuntime().availableProcessors());
+    }
+}
---
+++ Java-8-Parallel-Streams/src/parallelism_basics/pack/ParallelStreamsExample.java
@@ -0,0 +1,11 @@
+package parallelism_basics.pack;
+
+import java.util.stream.Stream;
+
+public class ParallelStreamsExample {
+
+    public static void main(String[] args) {
+        Integer sum = Stream.iterate(1, i -> i + 1).limit(10000).parallel().reduce(0, Integer::sum);
+        System.out.println(sum);
+    }
+}
